8573c84bc0b6091038a99ddeb9180bd3b68c5f54
LucianKaltz <9962217+LucianKaltz@users.noreply.github.com>
2019-01-23 18:59:35 +0800

    init solution

---
+++ 344. Reverse String/xixing.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: void Do not return anything, modify s in-place instead.
+        """
+        str_len = len(s)
+        if str_len <= 1:
+            return;
+        
+        for i in range(0, str_len//2):
+            if s[i] == s[str_len - i - 1]:
+                continue
+            s[i], s[str_len -1 -i] = s[str_len -1 -i], s[i]
