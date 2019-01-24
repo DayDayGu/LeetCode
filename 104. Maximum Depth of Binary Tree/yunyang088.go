db98873c44efaa4ad92ce62e740bdb2fdf2fb612
yunyang088 <yunyang088@gmail.com>
2019-01-24 15:12:26 +0800

    104 yunyang088 go solution

---
+++ 104. Maximum Depth of Binary Tree/yunyang088.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(1+maxDepth(root.Left), 1+maxDepth(root.Right))
+}
+
+func max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
