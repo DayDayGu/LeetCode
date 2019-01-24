d4a2e34f75db977bd05b17a47cf71c0777aff5e5
laxect <laxect39@gmail.com>
2019-01-24 11:43:06 +0800

    update gitignore and clean 412 code

--- 412. Fizz Buzz/gyara.rs
+++ 412. Fizz Buzz/gyara.rs
@@ -3,12 +3,12 @@ struct Solution {}
 impl Solution {
     pub fn fizz_buzz(n: i32) -> Vec<String> {
         let mut res = Vec::with_capacity(n as usize);
-        for i in 0..(n as usize) {
-            let s = match (i+1) % 15 {
+        for i in 1..n+1 {
+            let s = match i % 15 {
                 0 => String::from("FizzBuzz"),
                 3 | 6 | 9 | 12 => String::from("Fizz"),
                 5 | 10 => String::from("Buzz"),
-                _ => (i+1).to_string(),
+                _ => i.to_string(),
             };
             res.push(s);
         }
