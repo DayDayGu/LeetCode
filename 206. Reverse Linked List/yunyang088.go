f3eb6815b33469d2e5578d84b5b6b745f33b002b
yunyang088 <yunyang088@gmail.com>
2019-02-13 19:00:59 +0800

    206 yunyang088 go solution

---
+++ 206. Reverse Linked List/yunyang088.go
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	var pre *ListNode
+	for {
+		if head == nil {
+			break
+		}
+		var next = head.Next
+		head.Next = pre
+		pre = head
+		head = next
+	}
+	return pre
+}
+
+func reverseListR(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	var tmp = reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return tmp
+}
