cedd91c3ef06f539426ec72577cdcf397aea12e1
tao <marshadowcc@gmail.com>
2020-03-11 22:18:29 +0800

    wo hao cai a commit 238

---
+++ 238. Product of Array Except Self/xia.py
@@ -0,0 +1,17 @@
+class Solution:
+    # 这题没做出来
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+        answer = [0] * length
+        answer[0] = 1
+
+        for i in range(1, length):
+            answer[i] = nums[i - 1] * answer[i - 1]
+        
+        R = 1
+        for i in reversed(range(length)):
+            answer[i] = answer[i] * R 
+            R *= nums[i]
+
+        return answer
+            
