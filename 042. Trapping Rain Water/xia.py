effc3c93a589d4d14f29756b1d96072959b03108
tao <marshadowcc@gmail.com>
2020-03-21 21:54:50 +0800

    update 042 solution

--- 042. Trapping Rain Water/xia.py
+++ 042. Trapping Rain Water/xia.py
@@ -35,33 +35,53 @@
 #             else:
 #                 i += 1
 #         return sum(list(map(lambda buket: self._compute_area(buket[0], buket[1], heights), bukets)))
+# class Solution(object):
+#     def trap(self, height):
+#         """
+#         没做出来
+#         :type height: List[int]
+#         :rtype: int
+#         """
+#         if len(height) < 3:
+#             return 0
+#         left, right = [0], []
+#         max_l = 0
+#         for i in range(1, len(height)):
+#             max_l = max(max_l, height[i-1])
+#             left.append(max_l)
+#         max_r = 0
+#         for j in range(len(height)-1, -1, -1):
+#             if j == len(height)-1:
+#                 right.append(0)
+#             else:
+#                 max_r = max(max_r, height[j+1])
+#                 right.append(max_r)
+#         right = right[::-1]
+#         ans = 0
+#         for i in range(len(height)):
+#             curArea = (min(left[i], right[i]) - height[i])
+#             if curArea > 0:
+#                 ans +=curArea
+#         return ans
+
 class Solution(object):
     def trap(self, height):
         """
-        没做出来
+        更牛逼的解法
         :type height: List[int]
         :rtype: int
         """
-        if len(height) < 3:
+        if not height or len(height) < 3:
             return 0
-        left, right = [0], []
-        max_l = 0
-        for i in range(1, len(height)):
-            max_l = max(max_l, height[i-1])
-            left.append(max_l)
-        max_r = 0
-        for j in range(len(height)-1, -1, -1):
-            if j == len(height)-1:
-                right.append(0)
-            else:
-                max_r = max(max_r, height[j+1])
-                right.append(max_r)
-        right = right[::-1]
-        ans = 0
+        stack, ans = [], 0
         for i in range(len(height)):
-            curArea = (min(left[i], right[i]) - height[i])
-            if curArea > 0:
-                ans +=curArea
+            while stack and height[i] > height[stack[-1]]:
+                tmp = stack.pop()
+                if stack:
+                    w = i - 1 - stack[-1]
+                    h = min(height[i], height[stack[-1]]) - height[tmp]
+                    ans +=(w*h)
+            stack.append(i)
         return ans
 # test code 
 # s = Solution()
